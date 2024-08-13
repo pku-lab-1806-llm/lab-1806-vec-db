0de100bfe698c1d58108e46c6096aea96d5a90c4
yyhhenry <yyhhenry@foxmail.com>
2024-08-13 13:07:14 +0800

    Update distance

--- src/vec_set.rs
+++ src/vec_set.rs
@@ -7,57 +7,10 @@ use crate::binary_scalar::BinaryScalar;
 
 use anyhow::Result;
 
-use crate::config::{DataType, DistanceAlgorithm, VecDataConfig};
+use crate::config::{DataType, VecDataConfig};
 
 pub type Vector<ScalarType> = [ScalarType];
 
-pub trait Distance {
-    fn l2_distance(&self, other: &Self) -> f32;
-    fn cosine_distance(&self, other: &Self) -> f32;
-    fn distance(&self, other: &Self, algorithm: DistanceAlgorithm) -> f32 {
-        use DistanceAlgorithm::*;
-        match algorithm {
-            L2 => self.l2_distance(other),
-            Cosine => self.cosine_distance(other),
-        }
-    }
-}
-impl Distance for Vector<f32> {
-    fn l2_distance(&self, other: &Self) -> f32 {
-        self.iter()
-            .zip(other.iter())
-            .map(|(a, b)| (a - b).powi(2))
-            .sum::<f32>()
-    }
-    fn cosine_distance(&self, other: &Self) -> f32 {
-        let dot_product = self
-            .iter()
-            .zip(other.iter())
-            .map(|(a, b)| a * b)
-            .sum::<f32>();
-        let norm_self = self.iter().map(|v| v.powi(2)).sum::<f32>().sqrt();
-        let norm_other = other.iter().map(|v| v.powi(2)).sum::<f32>().sqrt();
-        1.0 - dot_product / (norm_self * norm_other)
-    }
-}
-impl Distance for Vector<u8> {
-    fn l2_distance(&self, other: &Self) -> f32 {
-        self.iter()
-            .zip(other.iter())
-            .map(|(a, b)| (*a as i32 - *b as i32).pow(2))
-            .sum::<i32>() as f32
-    }
-    fn cosine_distance(&self, other: &Self) -> f32 {
-        let dot_product = self
-            .iter()
-            .zip(other.iter())
-            .map(|(a, b)| (*a as i32 * *b as i32))
-            .sum::<i32>() as f32;
-        let norm_self = self.iter().map(|v| (*v as i32).pow(2)).sum::<i32>() as f32;
-        let norm_other = other.iter().map(|v| (*v as i32).pow(2)).sum::<i32>() as f32;
-        1.0 - dot_product / (norm_self.sqrt() * norm_other.sqrt())
-    }
-}
 #[derive(Debug, Clone)]
 pub struct VecSet<ScalarType> {
     pub dim: usize,
@@ -112,22 +65,6 @@ impl<T: BinaryScalar> VecSet<T> {
 pub enum TypedVecRef<'a> {
     Float32(&'a [f32]),
     UInt8(&'a [u8]),
-}
-impl Distance for TypedVecRef<'_> {
-    fn l2_distance(&self, other: &Self) -> f32 {
-        match (self, other) {
-            (Self::Float32(a), Self::Float32(b)) => a.l2_distance(b),
-            (Self::UInt8(a), Self::UInt8(b)) => a.l2_distance(b),
-            _ => panic!("Cannot calculate distance between different types of vectors."),
-        }
-    }
-    fn cosine_distance(&self, other: &Self) -> f32 {
-        match (self, other) {
-            (Self::Float32(a), Self::Float32(b)) => a.cosine_distance(b),
-            (Self::UInt8(a), Self::UInt8(b)) => a.cosine_distance(b),
-            _ => panic!("Cannot calculate distance between different types of vectors."),
-        }
-    }
 }
 #[derive(Debug, Clone)]
 pub enum TypedVecSet {
