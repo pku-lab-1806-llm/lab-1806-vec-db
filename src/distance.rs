d39599e2550630f2653a9ea830c09bdf4427a6c1
yyhhenry <yyhhenry@foxmail.com>
2024-08-29 21:10:05 +0800

    fix: better distance mod

--- src/distance.rs
+++ src/distance.rs
@@ -10,7 +10,7 @@ pub trait Distance {
     ///
     /// Range: `[0.0, +inf]`
     fn l2_sqr_distance(&self, other: &Self) -> f32;
-    /// L2 distance. *Automatically implemented.*
+    /// L2 distance.
     ///
     /// Range: `[0.0, +inf]`
     fn l2_distance(&self, other: &Self) -> f32 {
@@ -23,12 +23,6 @@ pub trait Distance {
     ///
     /// Range: `[0.0, 2.0]`
     fn cosine_distance(&self, other: &Self) -> f32;
-    /// Calculate distance using the specified algorithm.
-    ///
-    /// Internally calls `DistanceAlgorithm::distance`.
-    fn dynamic_distance(&self, other: &Self, algorithm: DistanceAlgorithm) -> f32 {
-        algorithm.distance(self, other)
-    }
 }
 impl<T: Scalar> Distance for [T] {
     fn l2_sqr_distance(&self, other: &Self) -> f32 {
@@ -82,8 +76,8 @@ pub enum DistanceAlgorithm {
 }
 
 impl DistanceAlgorithm {
-    /// Calculate distance between two vectors using the specified algorithm.
-    pub fn distance<T: Distance + ?Sized>(&self, a: &T, b: &T) -> f32 {
+    /// Calculate distance between two slices using the specified algorithm.
+    pub fn distance<T: Scalar>(&self, a: &[T], b: &[T]) -> f32 {
         match self {
             L2Sqr => a.l2_sqr_distance(b),
             L2 => a.l2_distance(b),
@@ -91,24 +85,9 @@ impl DistanceAlgorithm {
         }
     }
 
-    /// Alias for `distance`.
-    /// Calculate distance between two vectors using the specified algorithm.
-    pub fn d<T: Distance + ?Sized>(&self, a: &T, b: &T) -> f32 {
-        self.distance(a, b)
-    }
-
+    /// Alias of `distance()`.
     /// Calculate distance between two slices using the specified algorithm.
-    ///
-    /// This may help the type inference system.
-    pub fn distance_slice<T: Scalar>(&self, a: &[T], b: &[T]) -> f32 {
-        self.distance(a, b)
-    }
-
-    /// Alias for `distance_slice`.
-    /// Calculate distance between two slices using the specified algorithm.
-    ///
-    /// This may help the type inference system.
-    pub fn ds<T: Scalar>(&self, a: &[T], b: &[T]) -> f32 {
+    pub fn d<T: Scalar>(&self, a: &[T], b: &[T]) -> f32 {
         self.distance(a, b)
     }
 }
@@ -124,13 +103,13 @@ mod test {
     fn test_l2_sqr_distance() {
         let a = vec![1.0, 2.0, 3.0];
         let b = vec![4.0, 5.0, 6.0];
-        assert!((L2Sqr.ds(&a, &b) - 27.0_f32).abs() < EPSILON);
+        assert!((L2Sqr.d(&a, &b) - 27.0_f32).abs() < EPSILON);
     }
 
     #[test]
     fn test_cosine_distance() {
         let a = [1, 2, 3];
         let b = [2, 4, 6];
-        assert!((Cosine.ds(&a, &b) - 0.0_f32).abs() < EPSILON);
+        assert!((Cosine.d(&a, &b) - 0.0_f32).abs() < EPSILON);
     }
 }
