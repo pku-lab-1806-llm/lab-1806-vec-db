27a5d85d6785443ac6a3713e3559721657c7a5f6
yyhhenry <yyhhenry@foxmail.com>
2024-09-11 22:21:39 +0800

    feat: level

--- src/index_algorithm/hnsw_index.rs
+++ src/index_algorithm/hnsw_index.rs
@@ -1,5 +1,6 @@
 use std::ops::Index;
 
+use rand::Rng;
 use serde::{Deserialize, Serialize};
 
 use crate::{distance::DistanceAlgorithm, scalar::Scalar, vec_set::VecSet};
@@ -18,15 +19,18 @@ pub struct HNSWConfig {
 /// The inner configuration of the HNSW algorithm.
 /// Contains more computed values.
 #[derive(Debug, Clone, Serialize, Deserialize)]
-#[allow(non_snake_case)]
 pub struct HNSWInnerConfig {
     pub dim: usize,
     pub dist: DistanceAlgorithm,
     pub max_elements: usize,
+    pub m: usize,
+    pub max_m0: usize,
     pub ef_construction: usize,
-    pub M: usize,
-    pub max_M0: usize,
     pub ef: usize,
+    /// inv_log_m = 1 / ln(M)$.
+    ///
+    /// rand_level = floor(-ln(rand_uniform(0.0,1.0)) * inv_log_m)
+    pub inv_log_m: f32,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -54,40 +58,60 @@ pub struct HNSWIndex<T> {
     pub links_len: Vec<Vec<u32>>,
     /// The level of each vector. level is 0-indexed.
     pub vec_level: Vec<u32>,
+    /// The maximum level of the index.
     pub max_level: Option<usize>,
+    /// The enter point of the search.
     pub enter_point: Option<usize>,
 }
 impl<T: Scalar> HNSWIndex<T> {
+    fn rand_level(&self, rng: &mut impl Rng) -> usize {
+        let rand_uniform: f32 = rng.gen_range(0.0..1.0);
+        (-rand_uniform.ln() * self.config.inv_log_m).floor() as usize
+    }
     fn _get_links(&self, vec_idx: usize, level_idx: usize) -> &[u32] {
         let len = self.links_len[vec_idx][level_idx] as usize;
         if level_idx == 0 {
-            let start = vec_idx * self.config.max_M0;
+            let start = vec_idx * self.config.max_m0;
             &self.level0_links[start..start + len]
         } else {
             let vec_links = &self.other_links[vec_idx];
-            let start = self.config.M * (level_idx - 1);
+            let start = self.config.m * (level_idx - 1);
             &vec_links[start..start + len]
         }
     }
     fn _put_links(&mut self, vec_idx: usize, level_idx: usize, links: &[u32]) {
+        self.max_level = self
+            .max_level
+            .map_or(Some(level_idx), |level| Some(level.max(level_idx)));
         let len = links.len() as u32;
         self.links_len[vec_idx][level_idx] = len;
         if level_idx == 0 {
-            let start = vec_idx * self.config.max_M0;
+            let start = vec_idx * self.config.max_m0;
             self.level0_links[start..start + len as usize].clone_from_slice(links);
         } else {
             let vec_links = &mut self.other_links[vec_idx];
-            let start = self.config.M * (level_idx - 1);
+            let start = self.config.m * (level_idx - 1);
             vec_links[start..start + len as usize].clone_from_slice(links);
         }
     }
-    fn _push_only(&mut self, vec: &[T], level: usize) -> usize {
+    /// Push a vector to the index and initialize:
+    ///
+    /// vec_set, level0_links, other_links, links_len,
+    /// vec_level, max_level, enter_point.
+    ///
+    /// Returns the index of the vector. The links are initialized to empty.
+    fn push_init(&mut self, vec: &[T], level: usize) -> usize {
         let idx = self.vec_set.push(vec);
         self.level0_links
-            .extend_from_slice(&vec![0; self.config.max_M0]);
-        let links = vec![0; self.config.M * level];
-        self.other_links.push(links);
+            .extend_from_slice(&vec![0; self.config.max_m0]);
+        self.other_links.push(vec![0; self.config.m * level]);
+        self.links_len.push(vec![0; level]);
         self.vec_level.push(level as u32);
+
+        if self.max_level.map_or(true, |max_level| level > max_level) {
+            self.max_level = Some(level);
+            self.enter_point = Some(idx);
+        }
 
         idx
     }
@@ -111,18 +135,16 @@ impl<T: Scalar> IndexBuilder<T> for HNSWIndex<T> {
         self.config.dim
     }
     fn new(dim: usize, dist: DistanceAlgorithm, config: Self::Config) -> Self {
-        #[allow(non_snake_case)]
-        let M = config.M;
-        assert!(
-            M <= 10_000,
-            " M parameter exceeds 10_000 which may lead to adverse effects."
-        );
-        #[allow(non_snake_case)]
-        let max_M0 = 2 * M;
         let max_elements = config.max_elements;
-
+        let m = config.M;
+        assert!(
+            m <= 10_000,
+            " M parameter exceeds 10_000 may lead to adverse effects."
+        );
+        let max_m0 = m * 2;
+        let ef_construction = config.ef_construction.max(m);
         let ef = 10;
-        let ef_construction = config.ef_construction.max(config.M);
+        let inv_log_m = 1.0 / (m as f32).ln();
 
         let vec_set = VecSet::<T>::with_capacity(max_elements, dim);
         let level0_links = Vec::with_capacity(max_elements);
@@ -136,9 +158,10 @@ impl<T: Scalar> IndexBuilder<T> for HNSWIndex<T> {
                 dist,
                 max_elements,
                 ef_construction,
-                M,
-                max_M0,
+                m,
+                max_m0,
                 ef,
+                inv_log_m,
             },
             vec_set,
             level0_links,
@@ -149,7 +172,9 @@ impl<T: Scalar> IndexBuilder<T> for HNSWIndex<T> {
             enter_point: None,
         }
     }
-    fn add(&mut self, _vec: &[T], _rng: &mut impl rand::Rng) -> usize {
+    fn add(&mut self, vec: &[T], rng: &mut impl Rng) -> usize {
+        let level = self.rand_level(rng);
+        self.push_init(vec, level);
         unimplemented!("HNSWIndex::add")
     }
 }
