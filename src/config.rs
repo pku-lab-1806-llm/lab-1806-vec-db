0c0ce3128b768eb9459ebfaea767f479b4feea0d
yyhhenry <yyhhenry@foxmail.com>
2024-10-04 18:17:28 +0800

    fix: Remove gist_test.toml configuration file

--- src/config.rs
+++ src/config.rs
@@ -60,7 +60,7 @@ mod tests {
 
     #[test]
     fn test_load_from_toml_file() {
-        let config = VecDataConfig::load_from_toml_file("config/gist_test.toml").unwrap();
+        let config = VecDataConfig::load_from_toml_file("config/gist_1000.toml").unwrap();
         println!("{:#?}", config);
     }
 }
