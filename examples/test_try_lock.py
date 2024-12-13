2585af0925c6730884ba32acdbbc976179f09950
yyhhenry <yyhhenry@foxmail.com>
2024-12-13 21:45:57 +0800

    chore: bump version to 0.5.3

--- examples/test_try_lock.py
+++ examples/test_try_lock.py
@@ -1,4 +1,4 @@
 from lab_1806_vec_db import VecDB
 
 db1 = VecDB("./tmp/vec_db")
-db2 = VecDB("./tmp/vec_db")  # Failed to acquire lock
+db2 = VecDB("./tmp/vec_db")  # RuntimeError: Failed to acquire lock for VecDBManager
